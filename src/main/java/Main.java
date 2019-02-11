1fde2d1f4b59a334f59f44907cdbb921dd385274
Mithu Roy <roy.mithu66@gmail.com>
2019-02-11 07:28:45 +0530

    Creates observables in three different ways
    We've used static methods like just(), fromIterable() and create() method available in Observable class, to create Observables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,74 @@
+import io.reactivex.Observable;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/OyVIGm0O7yY
+ * https://youtu.be/0vqAcND_yas
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * In this class we've shown how easily you can get rid of anonymous inner class
- * using Lambdas and along with Method Reference the code looks much cleaner.
+ * In multiple different ways available we've shown three very common and important way
+ * to create Observables
  * <p>
- * Created By Mithu Roy on 20/01/2019
+ * Created By Mithu Roy on 09/02/2019
  */
 
 public class Main {
-
     public static void main(String[] args) {
-        /*
-         * Old School Implementation with anonymous inner class
-         * Thread thread = new Thread(new Runnable() {
-         *   @Override
-         *  public void run() {
-         *       runIt();
-         *   }
-         * });
-         */
-
-        // With Lambda and Method Reference
-        Thread thread = new Thread(Main::runIt);
-        thread.run();
+        createObservableWithJust();
+        createObservableFromIterable();
+        createObservableUsingCreate();
     }
 
-    private static void runIt() {
-        System.out.println("Hello World");
+    /**
+     * In this method we've created an integer observable
+     * using Observable's static method just()
+     * This method converts the items provided to an Observable
+     * So later we can subscribe and get the item one by one to take action
+     */
+    private static void createObservableWithJust() {
+        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
+
+        observable.subscribe(item -> System.out.println(item));
+    }
+
+    /**
+     * In this method we've created an integer observable
+     * using Observable's static method fromIterable()
+     * This method converts anything which extend or implement iterable, to an observable
+     * So later we can subscribe and get the item one by one to take action
+     */
+    private static void createObservableFromIterable() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Observable<Integer> observable = Observable.fromIterable(list);
+
+        observable.subscribe(item -> System.out.println(item));
+    }
+
+    /**
+     * In this method we've created an integer observable
+     * using Observable's static method create()
+     * Emitter here is an interface
+     * By calling it's onNext(), onComplete() and onError() method
+     * we can push events to our subscribers
+     * So our subscribers can subscribe and get the item one by one to take action
+     */
+    private static void createObservableUsingCreate() {
+        Observable<Integer> observable = Observable.create(emitter -> {
+            emitter.onNext(1);
+            emitter.onNext(2);
+            emitter.onNext(3);
+            emitter.onNext(4);
+            emitter.onNext(5);
+            emitter.onNext(null);
+            emitter.onComplete();
+        });
+
+        observable.subscribe(item -> System.out.println(item),
+                error -> System.out.println("There was error: " + error.getLocalizedMessage()),
+                () -> System.out.println("Completed"));
     }
 }
