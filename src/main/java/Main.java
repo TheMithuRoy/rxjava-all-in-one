b3c236c756a005f3263ec7cb99a10c8b01ed2e1d
Mithu Roy <roy.mithu66@gmail.com>
2019-03-03 11:17:06 +0530

    Creates Observable using Observable.error()
    ~ Creates the Observable by directly passing an instance of Exception
    ~ Creates the Observable by passing the instance of Callable interface which takes an instance of Exception through lambda

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,67 +1,50 @@
 import io.reactivex.Observable;
-import io.reactivex.observables.ConnectableObservable;
-
-import static java.lang.Thread.sleep;
 
 /**
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/cZRodTYKlUs
+ * https://youtu.be/bEti4Rqhbfw
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * We've created Cold Observable using Observable.just()
- * and paused the thread to prove whether it is really Cold Observable
- * and we also created Hot Observable using by converting a Cold Observable
- * to a Hot Observable using Connectable Observable
+ * We've created Observable using Observable.error()
+ * This factory method only provides exception to their Observers
+ * through the onError()
+ * This is specifically helpful if we don't want to throw exception directly,
+ * so we can handle it gracefully in the onError()
  * <p>
- * Created By Mithu Roy on 16/02/2019
+ * Created By Mithu Roy on 03/03/2019
  */
 
 public class Main {
     public static void main(String[] args) {
-        createColdObservable();
-        createHotAndConnectableObservable();
+        throwException();
+        throwExceptionUsingCallable();
     }
 
     /**
-     * Creates Cold Observable using Observable.just()
-     * Because each and every onNext() gets their separate emission
+     * This method uses Observable.error() to pass a new instance of exception directly
+     * So their observers get the same exception instance everytime
      */
-    private static void createColdObservable() {
-        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
-
-        observable.subscribe(item -> System.out.println("Observer 1: " + item));
-
-        pause(3000);
-
-        observable.subscribe(item -> System.out.println("Observer 2: " + item));
+    private static void throwException() {
+        Observable observable = Observable.error(new Exception("An Exception"));
+        observable.subscribe(System.out::println, error -> System.out.println("Error 1: " + error.hashCode()));
+        observable.subscribe(System.out::println, error -> System.out.println("Error 2: " + error.hashCode()));
     }
 
     /**
-     * Creates a Hot Observable
-     * The moment we call the publish() method on Observable.just()
-     * It Converts the Observable to a Connectable Observable
-     * Connectable Observable doesn't start it's emission right after you subscribe
-     * The moment we call connect() method it starts emission
-     * Any Observer which subscribes after calling connect() misses emissions
+     * This method uses Observable.error() to pass a new Instance of Callable
+     * which takes an Exception as it's return type through lambda
+     * So their Observers gets a new instance of exception on onError() every time
      */
-    private static void createHotAndConnectableObservable() {
-        ConnectableObservable<Integer> observable = Observable.just(1, 2, 3, 4, 5).publish();
-
-        observable.subscribe(item -> System.out.println("Observer 1: " + item));
-    }
-
-    /**
-     * This method sleep the main thread for specified duration
-     *
-     * @param duration Sleep Duration in Milliseconds
-     */
-    private static void pause(int duration) {
-        try {
-            sleep(duration);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private static void throwExceptionUsingCallable() {
+        Observable observable = Observable.error(() -> {
+            // We're printing this message to show that new instance gets created before
+            // publishing the error to their Observers
+            System.out.println("New Exception Created");
+            return new Exception("An Exception");
+        });
+        observable.subscribe(System.out::println, error -> System.out.println("Error 1: " + error.hashCode()));
+        observable.subscribe(System.out::println, error -> System.out.println("Error 2: " + error.hashCode()));
     }
 }
