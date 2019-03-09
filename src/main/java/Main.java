db541f25a9ed9f494936491434770dd8049d8716
Mithu Roy <roy.mithu66@gmail.com>
2019-03-09 08:46:10 +0530

    Creates Observable using Observable.empty() and Observable.never()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,58 @@
 import io.reactivex.Observable;
 
+import static java.lang.Thread.sleep;
+
 /**
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/bEti4Rqhbfw
+ * https://youtu.be/61wtJkWlx7w
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * We've created Observable using Observable.error()
- * This factory method only provides exception to their Observers
- * through the onError()
- * This is specifically helpful if we don't want to throw exception directly,
- * so we can handle it gracefully in the onError()
+ * We've created Observable using two factory methods i.e., Observable.empty() and Observable.never()
  * <p>
  * Created By Mithu Roy on 03/03/2019
  */
 
 public class Main {
     public static void main(String[] args) {
-        throwException();
-        throwExceptionUsingCallable();
+        createObservableUsingEmpty();
+        createObservableUsingNever();
     }
 
     /**
-     * This method uses Observable.error() to pass a new instance of exception directly
-     * So their observers get the same exception instance everytime
+     * Creates Observable using the factory method called empty()
+     * Which doesn't emit any item to onNext() and only completes immediately
+     * So, we get the callback on onComplete()
      */
-    private static void throwException() {
-        Observable observable = Observable.error(new Exception("An Exception"));
-        observable.subscribe(System.out::println, error -> System.out.println("Error 1: " + error.hashCode()));
-        observable.subscribe(System.out::println, error -> System.out.println("Error 2: " + error.hashCode()));
+    private static void createObservableUsingEmpty() {
+        Observable observable = Observable.empty();
+        observable.subscribe(System.out::println, System.out::println, () -> System.out.println("Completed"));
     }
 
     /**
-     * This method uses Observable.error() to pass a new Instance of Callable
-     * which takes an Exception as it's return type through lambda
-     * So their Observers gets a new instance of exception on onError() every time
+     * Creates Observable using the factory method called never()
+     * Which doesn't emit any item and never completes
+     * So, it's Observers are keep waiting until the thread is running
+     * Observable.never() is primarily used for testing purposes
      */
-    private static void throwExceptionUsingCallable() {
-        Observable observable = Observable.error(() -> {
-            // We're printing this message to show that new instance gets created before
-            // publishing the error to their Observers
-            System.out.println("New Exception Created");
-            return new Exception("An Exception");
-        });
-        observable.subscribe(System.out::println, error -> System.out.println("Error 1: " + error.hashCode()));
-        observable.subscribe(System.out::println, error -> System.out.println("Error 2: " + error.hashCode()));
+    private static void createObservableUsingNever() {
+        Observable observable = Observable.never();
+        observable.subscribe(System.out::println, System.out::println, () -> System.out.println("Completed"));
+        // Pause the main thread for the hope that it will print something
+        pause(3000);
+    }
+
+    /**
+     * This method sleep the main thread for specified duration
+     *
+     * @param duration Sleep Duration in Milliseconds
+     */
+    private static void pause(int duration) {
+        try {
+            sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
