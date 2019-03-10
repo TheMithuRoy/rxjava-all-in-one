ebfdfd75362b0336221feee352d0ca143cdb8e81
Mithu Roy <roy.mithu66@gmail.com>
2019-03-10 08:56:06 +0530

    Creates Observable using Observable.range()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,58 +1,25 @@
 import io.reactivex.Observable;
 
-import static java.lang.Thread.sleep;
-
 /**
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/61wtJkWlx7w
+ * https://youtu.be/MoMb2CMR6nU
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * We've created Observable using two factory methods i.e., Observable.empty() and Observable.never()
+ * We've created Observable using Observable.range()
  * <p>
- * Created By Mithu Roy on 03/03/2019
+ * Created By Mithu Roy on 09/03/2019
  */
 
 public class Main {
     public static void main(String[] args) {
-        createObservableUsingEmpty();
-        createObservableUsingNever();
-    }
+//        Observable<Integer> observable = Observable.range(0, 10);
+//        observable.subscribe(System.out::println);
 
-    /**
-     * Creates Observable using the factory method called empty()
-     * Which doesn't emit any item to onNext() and only completes immediately
-     * So, we get the callback on onComplete()
-     */
-    private static void createObservableUsingEmpty() {
-        Observable observable = Observable.empty();
-        observable.subscribe(System.out::println, System.out::println, () -> System.out.println("Completed"));
-    }
-
-    /**
-     * Creates Observable using the factory method called never()
-     * Which doesn't emit any item and never completes
-     * So, it's Observers are keep waiting until the thread is running
-     * Observable.never() is primarily used for testing purposes
-     */
-    private static void createObservableUsingNever() {
-        Observable observable = Observable.never();
-        observable.subscribe(System.out::println, System.out::println, () -> System.out.println("Completed"));
-        // Pause the main thread for the hope that it will print something
-        pause(3000);
-    }
-
-    /**
-     * This method sleep the main thread for specified duration
-     *
-     * @param duration Sleep Duration in Milliseconds
-     */
-    private static void pause(int duration) {
-        try {
-            sleep(duration);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // it prints from start up to (start + count - 1)
+        int start = 5, count = 2;
+        Observable<Integer> observable = Observable.range(start, count);
+        observable.subscribe(System.out::println);
     }
 }
