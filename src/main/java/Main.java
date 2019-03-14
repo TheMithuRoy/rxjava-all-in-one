4145977585b8b30347f1284c8b80e6a547db3d15
Mithu Roy <roy.mithu66@gmail.com>
2019-03-14 07:07:28 +0530

    Creates Observable using Observable.defer()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,22 +4,27 @@ import io.reactivex.Observable;
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/MoMb2CMR6nU
+ * https://youtu.be/RnU9nGJcbfo
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * We've created Observable using Observable.range()
+ * We've created Observable using Observable.defer()
+ * Which Delegates the item generation just before it reaches to the onNext()
+ * So that every Observers get a new instance, and stay updated with the new value
  * <p>
- * Created By Mithu Roy on 09/03/2019
+ * Created By Mithu Roy on 10/03/2019
  */
 
 public class Main {
-    public static void main(String[] args) {
-//        Observable<Integer> observable = Observable.range(0, 10);
-//        observable.subscribe(System.out::println);
+    private static int start = 5, count = 2;
 
+    public static void main(String[] args) {
         // it prints from start up to (start + count - 1)
-        int start = 5, count = 2;
-        Observable<Integer> observable = Observable.range(start, count);
-        observable.subscribe(System.out::println);
+        Observable<Integer> observable = Observable.defer(() -> {
+            System.out.println("New Observable is created with start = " + start + " and count = " + count);
+            return Observable.range(start, count);
+        });
+        observable.subscribe(item -> System.out.println("Observer 1: " + item));
+        count = 3;
+        observable.subscribe(item -> System.out.println("Observer 2: " + item));
     }
 }
