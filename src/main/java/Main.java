a0ee694c8d2dcccf92b6481e2196a36803422400
Mithu Roy <roy.mithu66@gmail.com>
2019-05-12 07:46:57 +0530

    Introduces take() takeWhile(), skip() and skipWhile()RxJava Operators ~ Also shows take() method overload which takes a time unit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,75 +1,96 @@
 import io.reactivex.Observable;
 
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
+
 /**
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/wrFvo7eNW6Q
+ * https://youtu.be/U1yPKXqCNoA
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
  * We've started our RxJava Operator's series with map() and filter()
- * Here you can check how map() and filter() works individually
- * and also with each other by chaining multiple operators
+ * Here we've used take and skip operator and some variants of them to showcase
+ * how they work and how skip is nothing but an opposite of take
  * <p>
- * Created By Mithu Roy on 28/04/2019
+ * Created By Mithu Roy on 05/05/2019
  */
 
 public class Main {
     public static void main(String[] args) {
-        mapOperator();
-        mapOperatorReturnsDifferentData();
-        filterOperator();
-        combineMapAndFilter();
+        takeOperator();
+        takeWithInterval();
+        takeWhileOperator();
+        skipOperator();
+        skipWhileOperator();
     }
 
     /**
-     * Uses the map() operator to transform the value in between,
-     * before it reaches to the Observer
+     * Used take(2) here, which emits only first 2 items and then complete
      */
-    private static void mapOperator() {
-        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
-        observable
-                .map(item -> item * 2)
+    private static void takeOperator() {
+        Observable.just(1,2,3,4,5)
+                .take(2)
+                .subscribe(System.out::println, System.out::println, () -> System.out.println("Completed"));
+    }
+
+    /**
+     * Used take(2) but with interval here, which emits items for the specified time interval only
+     */
+    private static void takeWithInterval() {
+        Observable.interval(300, TimeUnit.MILLISECONDS)
+                .take(2, TimeUnit.SECONDS)
+                .subscribe(System.out::println, System.out::println, () -> System.out.println("Completed"));
+
+        pause(5000);
+    }
+
+    /**
+     * This takeWhile() is like combination of filter and take,
+     * the only difference is filter goes through all the items to check if the logic is true
+     * whereas takeWhile() keep emitting only some logic is true
+     * and it completes once it gets logic as false
+     */
+    private static void takeWhileOperator() {
+        Observable.just(1,2,3,4,5,1,2,3,4,5)
+                .takeWhile(item -> item <= 3)
                 .subscribe(System.out::println);
     }
 
     /**
-     * Uses the map() operator to transform the value in between,
-     * before it reaches to the Observer and here map() emit different data type and
-     * Observer just needs to adjust with it or accept the same type of emission
+     * skip(2) is just the opposite of take(2)
+     * it will skip first values and emit remaining ones
      */
-    private static void mapOperatorReturnsDifferentData() {
-        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
-        observable
-                .map(item -> "Hello World!")
+    private static void skipOperator() {
+        Observable.just(1,2,3,4,5)
+                .skip(2)
                 .subscribe(System.out::println);
     }
 
     /**
-     * Uses the filter() operator to filter out the value in between,
-     * which doesn't meet the logic specified in filter,
-     * and filter() may not emit no item if it no item match that criteria
+     * skipWhile() is little is like combination of filter and skip,
+     * the only difference is filter goes through all the items to check if the logic is true
+     * whereas skipWhile() keep skipping items only if some logic true
+     * and once the logic is false it emits remaining items without checking
      */
-    private static void filterOperator() {
-        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
-        observable
-                .filter(item -> item % 2 == 0)
+    private static void skipWhileOperator() {
+        Observable.just(1,2,3,4,5,1,2,3,4,5)
+                .skipWhile(item -> item <= 3)
                 .subscribe(System.out::println);
     }
 
     /**
-     * Combines the map() and filter() operator together
-     * and as map() and filter() both are nothing but an Observable
-     * and also works like an Observer, so we can chain them,
-     * but the order of operation does matter here.
-     * Here filter() will kicks in first and map() will work on the filtered emission,
-     * and not the whole emission in general
+     * This method sleep the main thread for specified duration
+     *
+     * @param duration Sleep Duration in Milliseconds
      */
-    private static void combineMapAndFilter() {
-        Observable<Integer> observable = Observable.just(1, 2, 3, 4, 5);
-        observable
-                .filter(item -> item % 2 == 0)
-                .map(item -> item * 2)
-                .subscribe(System.out::println);
+    private static void pause(int duration) {
+        try {
+            sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
