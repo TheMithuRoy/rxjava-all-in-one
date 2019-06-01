12cb9ea6aab43150d1bff788a7efec5341b35124
Mithu Roy <roy.mithu66@gmail.com>
2019-06-01 10:31:20 +0530

    Introduces repeat() and scan() to repeat emission and perform operation with accumulator and next respectively

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,38 +5,47 @@ import io.reactivex.Observable;
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
  * https://youtu.be/6IwEvHH_BRk
- * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
+ * Subscribe here -> https://youtu.be/T10dfR83O4U
  * <p>
- * Here we've used defaultIfEmpty and switchIfEmpty to emit default value and switch Observable Sources respectively
+ * Here we've used repeat operator to repeat emissions
+ * and Scan operator to perform operations on accumulator and the next item
  * <p>
- * Created By Mithu Roy on 19/05/2019
+ * Created By Mithu Roy on 26/05/2019
  */
 
 public class Main {
     public static void main(String[] args) {
-        useDefaultIfEmpty();
-        useSwitchIfEmpty();
+        useRepeat();
+        useScan();
+        useScanWithInitialValue();
     }
 
     /**
-     * Used defaultIfEmpty() operator so the observer will emit at least a default value
-     * if the emission gets empty
+     * This used repeat operator to specify how many times emission witll repeat
      */
-    private static void useDefaultIfEmpty() {
-        Observable.just(1,2,3,4,5)
-                .filter(item -> item > 10)
-                .defaultIfEmpty(100)
+    private static void useRepeat() {
+        Observable.just(1, 2, 3, 4, 5)
+                .repeat(3)
                 .subscribe(System.out::println);
     }
 
     /**
-     * This will switch to some alternative Observable Source
-     * if the emission gets empty
+     * This uses scan operator to print the sum of the previously emitted item and current one that is going to emit
      */
-    private static void useSwitchIfEmpty() {
-        Observable.just(1,2,3,4,5)
-                .filter(item -> item > 10)
-                .switchIfEmpty(Observable.just(6,7,8,9,10))
+    private static void useScan() {
+        Observable.just(1, 2, 3, 4, 5)
+                .scan((accumulator, next) -> accumulator + next)
                 .subscribe(System.out::println);
     }
+
+    /**
+     * This uses scan operator print the sum of the previously emitted item and current one that is going to emit,
+     * but this also takes the initial emission into consideration by specifying an initial value
+     */
+    private static void useScanWithInitialValue() {
+        Observable.just(1, 2, 3, 4, 5)
+                .scan(10, (accumulator, next) -> accumulator + next)
+                .subscribe(System.out::println);
+    }
+
 }
