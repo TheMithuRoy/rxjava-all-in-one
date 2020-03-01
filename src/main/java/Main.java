fdec4f61383eae7f7f8dd6406ba732d4bc804bbb
Mithu Roy <roy.mithu66@gmail.com>
2020-03-02 00:19:14 +0530

    Introduces delay and delay with delayError flag to to delay the emission as well as error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import io.reactivex.Observable;
 
-import java.util.Comparator;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
 
 /**
  * This is an example of illustration of the video series RxJava All In One.
@@ -17,38 +19,47 @@ import java.util.Comparator;
 public class Main {
 
     public static void main(String[] args) {
-        useSorted();
-        useSortedWithOwnComparator();
-        useSortedOnNonComparator();
+        delay();
+        delayError();
     }
 
     /**
-     * This used sorted operator to sort the operator
+     * Used 'delay' operator to add a delay before the Observable start emission
+     * Note: 'delay' doesn't delay each emission, instead it delays the start of the emission
      */
-    private static void useSorted() {
-        Observable.just(3, 5, 2, 4, 1)
-                .sorted()
+    private static void delay() {
+        Observable.just(1, 2, 3, 4, 5)
+                .delay(3000, TimeUnit.MILLISECONDS)
                 .subscribe(System.out::println);
+
+        pause(5000);
     }
 
     /**
-     * This used sorted operator along with Comparators reverse function
-     * to sort and reverse the emission
+     * 'delay' operator doesn't add any delay before emitting error
+     * This means the error is immediately emitted to it's subscribers by default
+     * To delay the emission of error we need to pass delayError parameter as true
      */
-    private static void useSortedWithOwnComparator() {
-        Observable.just(3, 5, 2, 4, 1)
-                .sorted(Comparator.reverseOrder())
-                .subscribe(System.out::println);
+    private static void delayError() {
+        Observable.error(new Exception("Error"))
+                .delay(3, TimeUnit.SECONDS, true)
+                .subscribe(System.out::println,
+                        error -> System.out.println(error.getLocalizedMessage()),
+                        () -> System.out.println("Completed"));
+        pause(5000);
     }
 
     /**
-     * This used sorted operator along with Integer's compare function to
-     * sort the emission based on their length
+     * This method sleep the main thread for specified duration
+     *
+     * @param duration Sleep Duration in Milliseconds
      */
-    private static void useSortedOnNonComparator() {
-        Observable.just("foo", "john", "bar")
-                .sorted((first, second) -> Integer.compare(first.length(), second.length()))
-                .subscribe(System.out::println);
+    private static void pause(int duration) {
+        try {
+            sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
