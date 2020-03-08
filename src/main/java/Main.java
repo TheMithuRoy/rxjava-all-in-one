dc23c7afefb0b77b6c96df7614af1d22043e6b28
Mithu Roy <roy.mithu66@gmail.com>
2020-03-08 16:59:43 +0530

    Introduces contains operator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,64 +1,54 @@
 import io.reactivex.Observable;
 
-import java.util.concurrent.TimeUnit;
-
-import static java.lang.Thread.sleep;
-
 /**
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/jnxoOup1eY0
+ * https://youtu.be/vjbTOReGZN4
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * Here we've used sorted operator to sort the emission based on different criteria
+ * Here we've used contains operator to check if an emission contains the specific item or not
  * <p>
- * Created By Mithu Roy on 02/06/2019
+ * Created By Mithu Roy on 08/02/2020
  */
 
 public class Main {
 
     public static void main(String[] args) {
-        delay();
-        delayError();
+        containsWithPremitive();
+        containsWithNonPremitive();
     }
 
     /**
-     * Used 'delay' operator to add a delay before the Observable start emission
-     * Note: 'delay' doesn't delay each emission, instead it delays the start of the emission
+     * contains operator checks if the number exist in the Observable emission
+     * As soon as it gets the item it emits true or false otherwise
      */
-    private static void delay() {
+    private static void containsWithPremitive() {
         Observable.just(1, 2, 3, 4, 5)
-                .delay(3000, TimeUnit.MILLISECONDS)
+                .contains(3)
                 .subscribe(System.out::println);
-
-        pause(5000);
     }
 
     /**
-     * 'delay' operator doesn't add any delay before emitting error
-     * This means the error is immediately emitted to it's subscribers by default
-     * To delay the emission of error we need to pass delayError parameter as true
+     * contains operator checks if the specific object exist in the Observable emission
+     * based on the Object's hashcode
+     * As soon as it gets the item it emits true or false otherwise
      */
-    private static void delayError() {
-        Observable.error(new Exception("Error"))
-                .delay(3, TimeUnit.SECONDS, true)
-                .subscribe(System.out::println,
-                        error -> System.out.println(error.getLocalizedMessage()),
-                        () -> System.out.println("Completed"));
-        pause(5000);
+    private static void containsWithNonPremitive() {
+        User user = new User("mroydroid");
+        Observable.just(user)
+                .contains(user)
+                .subscribe(System.out::println);
     }
 
     /**
-     * This method sleep the main thread for specified duration
-     *
-     * @param duration Sleep Duration in Milliseconds
+     * a static class for demonstration purpose
      */
-    private static void pause(int duration) {
-        try {
-            sleep(duration);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    static class User {
+        String name;
+
+        User(String name) {
+            this.name = name;
         }
     }
 
