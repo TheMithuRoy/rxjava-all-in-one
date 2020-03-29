af63596f39ec09ca9627360945c08f56bc21d911
Mithu Roy <roy.mithu66@gmail.com>
2020-03-29 17:31:53 +0530

    Introduces 4 error handling operators doOnError, onErrorResumeNext, onErrorReturn and onErrorReturnItem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
 import io.reactivex.Observable;
 
+import java.io.IOException;
+
 /**
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/vjbTOReGZN4
+ * https://youtu.be/2MEOCo_zz1E
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * Here we've used contains operator to check if an emission contains the specific item or not
+ * Here we've 4 error handling operators to showcase some techniques to handle errors in RxJava
  * <p>
  * Created By Mithu Roy on 08/02/2020
  */
@@ -15,41 +17,65 @@ import io.reactivex.Observable;
 public class Main {
 
     public static void main(String[] args) {
-        containsWithPremitive();
-        containsWithNonPremitive();
+        exDoOnError();
+        exOnErrorResumeNext();
+        exOnErrorReturn();
+        exOnErrorReturnItem();
     }
 
     /**
-     * contains operator checks if the number exist in the Observable emission
-     * As soon as it gets the item it emits true or false otherwise
+     * The error goes to the doOnError lambda on the chain first, so we can handle it
      */
-    private static void containsWithPremitive() {
-        Observable.just(1, 2, 3, 4, 5)
-                .contains(3)
-                .subscribe(System.out::println);
+    private static void exDoOnError() {
+        Observable.error(new Exception("This is an example error"))
+                .doOnError(error -> System.out.println("Error: " + error.getMessage()))
+                .subscribe(
+                        System.out::println,
+                        error -> System.out.println("Subscribed Error: " + error.getMessage()),
+                        () -> System.out.println("Completed"));
     }
 
     /**
-     * contains operator checks if the specific object exist in the Observable emission
-     * based on the Object's hashcode
-     * As soon as it gets the item it emits true or false otherwise
+     * Whenever an error is found on the chain, it goes to the onErrorResumeNext
+     * As this takes another Observable, the subscriber switch to that Observable
+     * to skip the error
      */
-    private static void containsWithNonPremitive() {
-        User user = new User("mroydroid");
-        Observable.just(user)
-                .contains(user)
-                .subscribe(System.out::println);
+    private static void exOnErrorResumeNext() {
+        Observable.error(new Exception("This is an example error"))
+                .onErrorResumeNext(Observable.just(1, 2, 3, 4, 5))
+                .subscribe(
+                        System.out::println,
+                        error -> System.out.println("Subscribed Error: " + error.getMessage()),
+                        () -> System.out.println("Completed"));
     }
 
     /**
-     * a static class for demonstration purpose
+     * We can return specific values based on the error type
+     * As when we get the error it goes to the onErrorReturn lambda
      */
-    static class User {
-        String name;
+    private static void exOnErrorReturn() {
+        Observable.error(new Exception("This is an example error"))
+                .onErrorReturn(error -> {
+                    if (error instanceof IOException) return 0;
+                    else throw new Exception("This is an exception");
+                })
+                .subscribe(
+                        System.out::println,
+                        error -> System.out.println("Subscribed Error: " + error.getMessage()),
+                        () -> System.out.println("Completed"));
+    }
 
-        User(String name) {
-            this.name = name;
-        }
+    /**
+     * We can pass an alternative for the subscriber below the chain
+     * Whenever error encounters it gives that specific alternative
+     */
+    private static void exOnErrorReturnItem() {
+        Observable.error(new IOException("This is an example error"))
+                .onErrorReturnItem(1)
+                .subscribe(
+                        System.out::println,
+                        error -> System.out.println("Subscribed Error: " + error.getMessage()),
+                        () -> System.out.println("Completed"));
     }
 
 }
