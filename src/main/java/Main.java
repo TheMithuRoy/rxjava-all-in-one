4190ac3fb4e2aa0f66242e44f2cd2d3006913170
Mithu Roy <roy.mithu66@gmail.com>
2020-04-05 11:58:54 +0530

    Introduces 3 error handling operators retry and retryUntil

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,63 +1,39 @@
 import io.reactivex.Observable;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/2MEOCo_zz1E
+ * https://youtu.be/WA3TdQEoKOo
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * Here we've 4 error handling operators to showcase some techniques to handle errors in RxJava
+ * Here we've 2 error handling operators to showcase some techniques to handle errors in RxJava
  * <p>
- * Created By Mithu Roy on 08/02/2020
+ * Created By Mithu Roy on 29/03/2020
  */
 
 public class Main {
 
     public static void main(String[] args) {
-        exDoOnError();
-        exOnErrorResumeNext();
-        exOnErrorReturn();
-        exOnErrorReturnItem();
+        retryWithPredicate();
+        exRetry();
+        exRetryUntil();
     }
 
     /**
-     * The error goes to the doOnError lambda on the chain first, so we can handle it
+     * This retry block try to analyze the error and take decision based on the error whether to retry or not
+     * based on our logic inside that block
      */
-    private static void exDoOnError() {
-        Observable.error(new Exception("This is an example error"))
-                .doOnError(error -> System.out.println("Error: " + error.getMessage()))
-                .subscribe(
-                        System.out::println,
-                        error -> System.out.println("Subscribed Error: " + error.getMessage()),
-                        () -> System.out.println("Completed"));
-    }
-
-    /**
-     * Whenever an error is found on the chain, it goes to the onErrorResumeNext
-     * As this takes another Observable, the subscriber switch to that Observable
-     * to skip the error
-     */
-    private static void exOnErrorResumeNext() {
-        Observable.error(new Exception("This is an example error"))
-                .onErrorResumeNext(Observable.just(1, 2, 3, 4, 5))
-                .subscribe(
-                        System.out::println,
-                        error -> System.out.println("Subscribed Error: " + error.getMessage()),
-                        () -> System.out.println("Completed"));
-    }
-
-    /**
-     * We can return specific values based on the error type
-     * As when we get the error it goes to the onErrorReturn lambda
-     */
-    private static void exOnErrorReturn() {
-        Observable.error(new Exception("This is an example error"))
-                .onErrorReturn(error -> {
-                    if (error instanceof IOException) return 0;
-                    else throw new Exception("This is an exception");
+    private static void retryWithPredicate() {
+        Observable.error(new IOException("This is an example error"))
+                .retry(error -> {
+                    if (error instanceof IOException) {
+                        System.out.println("retrying");
+                        return true;
+                    } else return false;
                 })
                 .subscribe(
                         System.out::println,
@@ -66,16 +42,34 @@ public class Main {
     }
 
     /**
-     * We can pass an alternative for the subscriber below the chain
-     * Whenever error encounters it gives that specific alternative
+     * This retry takes the number and tries to retry subscribing and getting the data from the observable again
      */
-    private static void exOnErrorReturnItem() {
-        Observable.error(new IOException("This is an example error"))
-                .onErrorReturnItem(1)
+    private static void exRetry() {
+        Observable.error(new Exception("This is an example error"))
+                .retry(3)
                 .subscribe(
                         System.out::println,
                         error -> System.out.println("Subscribed Error: " + error.getMessage()),
                         () -> System.out.println("Completed"));
     }
 
+    /**
+     * retryUntil depends on the boolean that we pass, it keeps retrying until we pass true based on the logic
+     */
+    private static void exRetryUntil() {
+        AtomicInteger atomicInteger = new AtomicInteger();
+        Observable.error(new Exception("This is an example error"))
+                .doOnError(error -> {
+                    System.out.println(atomicInteger.get());
+                    atomicInteger.getAndIncrement();
+                })
+                .retryUntil(() -> {
+                    System.out.println("Retrying");
+                    return atomicInteger.get() >= 3;
+                })
+                .subscribe(
+                        System.out::println,
+                        error -> System.out.println("Subscribed Error: " + error.getMessage()),
+                        () -> System.out.println("Completed"));
+    }
 }
