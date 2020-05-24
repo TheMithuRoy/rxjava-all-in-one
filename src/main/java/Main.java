cbddd715cc1a563c654243f9fbf528c8b6e768a4
Mithu Roy <roy.mithu66@gmail.com>
2020-05-24 10:56:31 +0530

    Introduces flatMap and flatMap with biFunction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,65 +1,44 @@
 import io.reactivex.Observable;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/e5Y5EAFuLjw
+ * https://youtu.be/d-LV8ayemQ8
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * Here we're Using zip, zipWith and zipIterable operator to merge the emission in the Zipper function
+ * Here we're Using flatMap and flatMap with biFunction
  * <p>
- * Created By Mithu Roy on 26/04/2020
+ * Created By Mithu Roy on 03/05/2020
  */
 
 public class Main {
 
     public static void main(String[] args) {
-        exZip();
-        exZipWith();
-        exZipIterable();
+        exFlatMap();
+        exFlatMapBiFunction();
     }
 
     /**
-     * Uses Zip operator to get the stream on the Zipper function
+     * This uses the flatMap to return desired Observables to the downstream
+     * based on upstream emissions it gets in the Function
      */
-    private static void exZip() {
-        Observable<Integer> oneToFive = Observable.just(1, 2, 3, 4, 5);
-        Observable<Integer> sixToTen = Observable.range(6, 5);
-        Observable<Integer> elevenToFifteen = Observable.fromIterable(Arrays.asList(11, 12, 13, 14, 15));
-
-        Observable.zip(oneToFive, sixToTen, elevenToFifteen, (a, b, c) -> a + b + c)
-                .subscribe(System.out::println);
-
+    private static void exFlatMap() {
+        Observable<String> observable = Observable.just("foo", "bar", "jam");
+        observable.flatMap((string) -> {
+            if (string.equals("bar")) return Observable.empty();
+            return Observable.fromArray(string.split(""));
+        }).subscribe(System.out::println);
     }
 
     /**
-     * Uses ZipWith operator on the Observable to easily zip One Observable with another
+     * This is similar to normal flatMap except the biFunction
+     * which merge upstream emission with the emissions from the flatMap returned Observables from Function
      */
-    private static void exZipWith() {
-        Observable<Integer> oneToFive = Observable.just(1, 2, 3, 4, 5);
-        Observable<Integer> sixToTen = Observable.fromIterable(Arrays.asList(6, 7, 8, 9, 10));
-
-        oneToFive.zipWith(sixToTen, Integer::sum)
-                .subscribe(System.out::println);
-    }
-
-    /**
-     * Uses zipIterable operator which takes List of Observables and provides the zipped emission in an array
-     */
-    private static void exZipIterable() {
-        Observable<Integer> oneToFive = Observable.just(1, 2, 3, 4, 5);
-        Observable<Integer> sixToTen = Observable.just(6, 7, 8, 9, 10);
-        Observable<Integer> elevenToFifteen = Observable.just(11, 12, 13, 14, 15);
-
-        List<Observable<Integer>> observables = Arrays.asList(
-                oneToFive, sixToTen, elevenToFifteen
-        );
-
-        Observable.zipIterable(observables, Arrays::toString, true, 1)
+    private static void exFlatMapBiFunction() {
+        Observable<String> observable = Observable.just("foo", "bar", "jam");
+        observable.flatMap(string -> Observable.fromArray(string.split("")),
+                (actual, second) -> actual + " " + second)
                 .subscribe(System.out::println);
     }
 }
