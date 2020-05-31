2a8c5055626c60485065cb67d96723d42cb2860f
Mithu Roy <roy.mithu66@gmail.com>
2020-05-31 18:46:54 +0530

    Introduces the difference between map and flatMap operators

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,41 +4,47 @@ import io.reactivex.Observable;
  * This is an example of illustration of the video series RxJava All In One.
  * <p>
  * You can watch the video implementation of this source code for free on YouTube here:
- * https://youtu.be/d-LV8ayemQ8
+ * https://youtu.be/NkI9XVB8i-8
  * Subscribe here -> http://bit.ly/MithuRoyOnYoutube
  * <p>
- * Here we're Using flatMap and flatMap with biFunction
+ * Here we're showing the difference between map and flatMap operators
  * <p>
- * Created By Mithu Roy on 03/05/2020
+ * Created By Mithu Roy on 24/05/2020
  */
 
 public class Main {
 
     public static void main(String[] args) {
+        exMap();
         exFlatMap();
-        exFlatMapBiFunction();
+        exMapWithObservable();
     }
 
     /**
-     * This uses the flatMap to return desired Observables to the downstream
-     * based on upstream emissions it gets in the Function
+     * This uses the map operators to change the emission in between before it reaches to it's subscriber
+     */
+    private static void exMap() {
+        Observable<String> observable = Observable.just("foo", "bard", "james");
+        observable
+                .map(item -> item.toUpperCase())
+                .subscribe(System.out::println);
+    }
+
+    /**
+     * This uses the flatMap to merge returned observables based on the emission
      */
     private static void exFlatMap() {
         Observable<String> observable = Observable.just("foo", "bar", "jam");
-        observable.flatMap((string) -> {
-            if (string.equals("bar")) return Observable.empty();
-            return Observable.fromArray(string.split(""));
-        }).subscribe(System.out::println);
+        observable.flatMap(item -> Observable.fromArray(item.split("")))
+                .subscribe(System.out::println);
     }
 
     /**
-     * This is similar to normal flatMap except the biFunction
-     * which merge upstream emission with the emissions from the flatMap returned Observables from Function
+     * This uses map operators to convert the stream in between to Observables
+     * and for obvious reasons the subscribers get the Observable object instead
      */
-    private static void exFlatMapBiFunction() {
+    private static void exMapWithObservable() {
         Observable<String> observable = Observable.just("foo", "bar", "jam");
-        observable.flatMap(string -> Observable.fromArray(string.split("")),
-                (actual, second) -> actual + " " + second)
-                .subscribe(System.out::println);
+        observable.map(item -> Observable.fromArray(item.split(""))).subscribe(System.out::println);
     }
 }
